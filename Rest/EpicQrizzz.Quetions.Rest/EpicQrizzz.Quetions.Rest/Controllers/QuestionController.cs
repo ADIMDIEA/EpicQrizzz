f2b45d7f1b96db3025374de4754222997674bcbd
LLDJ07 <Lldj07@hotmail.com>
2025-09-05 14:01:51 +0200

    conection db update

--- Rest/EpicQrizzz.Quetions.Rest/EpicQrizzz.Quetions.Rest/Controllers/QuestionController.cs
+++ Rest/EpicQrizzz.Quetions.Rest/EpicQrizzz.Quetions.Rest/Controllers/QuestionController.cs
@@ -8,7 +8,7 @@ namespace MyBackend.Controllers
     [Route("api/[controller]")]
     public class QuetionController : ControllerBase
     {
-        string connectionString = "Host=localhost;Port=3306;Database=EpicQrizzz;User=root;Password=;";
+        string connectionString = "server=joost.assenbergh.nl;user id=Joost;password=O%26tw%3A%60%22J%3BLmz7.x*X$!SW%60bbIi%2B%3A%5E7z_;database=EpicQrizzz";
 
         [HttpGet("GetAll")]
         public IActionResult GetAll()
