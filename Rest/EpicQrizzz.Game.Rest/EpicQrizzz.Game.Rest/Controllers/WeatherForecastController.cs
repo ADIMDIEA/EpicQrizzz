36ad0220bac9ded427d39c262e3575c52780c8f4
LLDJ07 <Lldj07@hotmail.com>
2025-09-18 11:23:20 +0200

    Added Game

---
+++ Rest/EpicQrizzz.Game.Rest/EpicQrizzz.Game.Rest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using EpicQrizzz.Game.Rest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpicQrizzz.Game.Rest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<GameModel> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new GameModel
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Rest/EpicQrizzz.Game.Rest/EpicQrizzz.Game.Rest/Models/GameModel.cs
@@ -0,0 +1,13 @@
+namespace EpicQrizzz.Game.Rest.Models
+{
+    public class GameModel
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
