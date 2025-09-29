113dbfde053e583132df39d2648770cffd977af1
laurensgithub1 <207018@student.aventus.nl>
2025-09-29 16:49:32 +0200

    Edim voor jou (:

--- Web/project/assets/js/home.js
+++ Web/project/assets/js/home.js
@@ -1,4 +1,4 @@
-
+const backendBase = "http://joost.assenbergh.nl:5292/api/User";
 
 const avatarDisplay = document.getElementById("avatar-display");
 const adminBtn = document.getElementById("admin-btn");
@@ -7,6 +7,22 @@ const logoutBtn = document.getElementById("logout-btn");
 // Huidige gebruiker ophalen (sessie/cookie uit backend)
 async function loadUser() {
   try {
+
+    const res = await fetch(`${backendBase}/GetById/${sessionStorage.getItem("epicqrizzz-userId")}`);
+    if (!res.ok) throw new Error("Netwerkfout");
+    const uuid = await res.json();
+    if (!uuid) throw new Error("Geen gebruiker gevonden");
+    const username = sessionStorage.getItem("epicqrizzz-username");
+
+    // Zoek de span op
+    const welcomeEl = document.getElementById("welcome-text");
+
+    // Pas de tekst aan
+    if (username) {
+      welcomeEl.textContent = `🙂 Hallo, ${username}!`;
+    } else {
+      welcomeEl.textContent = "🙂 Hallo, gebruiker!";
+    }
 
     if (!sessionStorage.getItem("epicqrizzz-userId")) {
       // niet ingelogd, terug naar login
