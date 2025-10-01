9d3c20d8fd02f4d04d077f94b848a3472a86f71e
laurensgithub1 <207018@student.aventus.nl>
2025-10-01 23:58:53 +0200

    multiplayer en js en html fixes

--- Web/project/assets/js/store.js
+++ Web/project/assets/js/store.js
@@ -9,14 +9,6 @@ document.querySelectorAll('.buyPackBtn').forEach(button => {
         }
 
         try {
-            const userResponse = await fetch(`http://joost.assenbergh.nl:5292/api/user/GetById/${userId}`);
-            const userData = await userResponse.json();
-
-            if (userData.munten < 5) {
-                alert('Je hebt niet genoeg munten om dit pack te kopen!');
-                return;
-            }
-
             const buyResponse = await fetch(`http://joost.assenbergh.nl:5294/api/store/BuyPack/${packId}/${userId}`, {
                 method: 'POST'
             });
@@ -24,6 +16,8 @@ document.querySelectorAll('.buyPackBtn').forEach(button => {
             if (buyResponse.ok) {
                 const buyData = await buyResponse.json();
                 alert(`Pack gekocht! Item ID: ${buyData.itemId}`);
+            } else if (buyResponse.status === 601) {
+                alert('Je hebt niet genoeg munten om dit pack te kopen.');
             } else {
                 const err = await buyResponse.text();
                 alert('Fout bij kopen van pack: ' + err);
