727554269423b033fdd0802e50b9e3a88477ece5
LLDJ07 <Lldj07@hotmail.com>
2025-10-01 16:14:16 +0200

    blablabla

--- Rest/EpicQrizzz.Game.Rest/EpicQrizzz.Game.Rest/Program.cs
+++ Rest/EpicQrizzz.Game.Rest/EpicQrizzz.Game.Rest/Program.cs
@@ -1,9 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// ✅ Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,6 +28,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ✅ Use CORS before authorization
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
